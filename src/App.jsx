6f8d44f94c1dc8f0a2d79e90cc9adb6382c859c5
Shivangi Singh <shivangiarusingh@gmail.com>
2025-11-09 18:51:08 +0530

    Made the Products list dynamic.

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,98 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+    const [productsArray, setProductsArray] = useState([
+        { id: 1, name: "Product-1", price: 100, quantity: 0 },
+        { id: 2, name: "Product-2", price: 200, quantity: 0 },
+        { id: 3, name: "Product-3", price: 300, quantity: 0 },
+        { id: 4, name: "Product-4", price: 400, quantity: 0 },
+        { id: 5, name: "Product-5", price: 500, quantity: 0 },
+    ]);
 
-  return (
-    <div className="container">
-      <div className="leftContainer">
-        <h2>Products</h2>
-        <div className="productsContainer">
-          <div className="product">
-            <p className='productName'>Product-1</p>
-            <p className='price'>100</p>
-            <div className="buttonContainer">
-              <button className="plus">+</button>
-              <p className='quantity'>2</p>
-              <button className="minus">-</button>
+    const [cartArray, setCartArray] = useState([
+        { id: 1, name: "Product-1", price: 100, quantity: 0 },
+        { id: 2, name: "Product-2", price: 200, quantity: 0 },
+        { id: 3, name: "Product-3", price: 300, quantity: 0 },
+        { id: 4, name: "Product-4", price: 400, quantity: 0 },
+        { id: 5, name: "Product-5", price: 500, quantity: 0 },
+    ]);
+
+    function increment(id) {
+        setProductsArray((productsArray) =>
+            productsArray.map((product) =>
+                (product.id === id)
+                    ? { ...product, quantity: product.quantity + 1 }
+                    : product
+            )
+        );
+
+        setCartArray([]);
+    }
+
+    function decrement(id) {
+        setProductsArray((productsArray) =>
+            productsArray.map((product) =>
+                (product.id === id && product.quantity >0)
+                    ? { ...product, quantity: product.quantity - 1 }
+                    : product
+            )
+        );
+    }
+
+    return (
+        <div className="container">
+            <div className="leftContainer">
+                <h2>Products</h2>
+                <div className="productsContainer">
+                    {productsArray.map((product) => (
+                        <div className="product" key={product.id}>
+                            <p className="productName">{product.name}</p>
+                            <p className="price">{product.price}</p>
+                            <div className="buttonContainer">
+                                <button
+                                    className="plus"
+                                    onClick={() => {
+                                        increment(product.id);
+                                    }}
+                                >
+                                    +
+                                </button>
+                                <p className="quantity">{product.quantity}</p>
+                                <button
+                                    className="minus"
+                                    onClick={() => {
+                                        decrement(product.id);
+                                    }}
+                                >
+                                    -
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
-          </div>
-          <div className="product">
-            <p className='productName'>Product-1</p>
-            <p className='price'>100</p>
-            <div className="buttonContainer">
-              <button className="plus">+</button>
-              <p className='quantity'>2</p>
-              <button className="minus">-</button>
+            <div className="rightContainer">
+                <h2>Cart</h2>
+                <div className="cartContainer">
+                    <div className="cartItems">
+                        {cartArray.map((item) => (
+                            <div className="item">
+                                <p>{item.name}</p>
+                                <p>
+                                    {item.quantity} x {item.price}
+                                </p>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="total">
+                        <p>Total:</p>
+                        <p>800</p>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className="product">
-            <p className='productName'>Product-1</p>
-            <p className='price'>100</p>
-            <div className="buttonContainer">
-              <button className="plus">+</button>
-              <p className='quantity'>2</p>
-              <button className="minus">-</button>
-            </div>
-          </div>
-          <div className="product">
-            <p className='productName'>Product-1</p>
-            <p className='price'>100</p>
-            <div className="buttonContainer">
-              <button className="plus">+</button>
-              <p className='quantity'>2</p>
-              <button className="minus">-</button>
-            </div>
-          </div>
-          <div className="product">
-            <p className='productName'>Product-1</p>
-            <p className='price'>100</p>
-            <div className="buttonContainer">
-              <button className="plus">+</button>
-              <p className='quantity'>2</p>
-              <button className="minus">-</button>
-            </div>
-          </div>
         </div>
-      </div>
-      <div className="rightContainer">
-        <h2>Cart</h2>
-        <div className="cartContainer">
-          <div className="cartItems">
-            <div className="item">
-              <p>Product-1</p>
-              <p>2*100</p>
-            </div>
-             <div className="item">
-              <p>Product-1</p>
-              <p>2*100</p>
-            </div>
-             <div className="item">
-              <p>Product-1</p>
-              <p>2*100</p>
-            </div>
-             <div className="item">
-              <p>Product-1</p>
-              <p>2*100</p>
-            </div>
-             <div className="item">
-              <p>Product-1</p>
-              <p>2*100</p>
-            </div>
-          </div>
-          <div className="total">
-            <p>Total:</p>
-            <p>800</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+    );
 }
 
-export default App
+export default App;
